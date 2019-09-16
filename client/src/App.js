aa4d144d2370516a9613a104d561a8e11a62b575
Brandy <brandybeckerfsw@gmail.com>
2019-09-16 16:52:11 -0400

    MVP AND DRY CODE STRETCH AYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYE

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
-
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import Movie from "./Movies/Movie";
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/Movies/:id" component={Movie} />
+      </div>
     </div>
   );
 };
